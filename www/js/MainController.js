0e9d7b7ef005468e2e1921ff99e1760e0480567f
Vincent Wong <vincentw@live.ca>
2016-11-20 21:33:46 -0800

    Fetch exchange rates from API. If fail, fallback to hardcoded rates.

--- www/js/MainController.js
+++ www/js/MainController.js
@@ -1,17 +1,22 @@
-angular.module('app').controller('MainController', ['$scope', function($scope) {
-  $scope.currencies = ["cad", "usd", "jpy", "krw"];
+angular.module('app').controller('MainController', ['$scope', '$http', '$ionicLoading', function($scope, $http, $ionicLoading) {
+  /* To add a new currency, add it to $scope.currencies and oneUSDTo, and $scope.currenciesModel.
+  * update oneUSDTo values for all currencies, and update $scope.exchangeRateLastUpdated value */
+  $scope.currencies = ["CAD", "USD", "JPY", "KRW"];
   var oneUSDTo = {
-    "cad" : 1.350550,
-    "jpy" : 110.877000,
-    "krw" : 1181.645000,
-    "usd" : 1
+    "CAD" : 1.350550,
+    "JPY" : 110.877000,
+    "KRW" : 1181.645000,
+    "USD" : 1
   };
 
+  // month is month-1. yyyy, mm, day, hours, minutes, seconds, milliseconds
+  $scope.exchangeRateLastUpdated = new Date(Date.UTC (2016, 10, 19, 12, 0, 0));
+
   $scope.currenciesModel = {
-    "cad" : null,
-    "usd" : null,
-    "jpy" : null,
-    "krw" : null
+    "CAD" : null,
+    "JPY" : null,
+    "KRW" : null,
+    "USD" : null
   };
 
   //$scope.checkbox.visa = true;
@@ -45,4 +50,61 @@ angular.module('app').controller('MainController', ['$scope', function($scope) {
   function round(value, decimals) {
     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
   }
+
+
+
+  /* Loading spinner */
+
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: '<ion-spinner icon="dots" class="spinner-light"></ion-spinner>'
+    });
+  };
+
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+
+
+  /* Load exchange rates (api), otherwise use hardcoded rates */
+  $scope.getExchangeRates = function() {
+    // Could convert to a service, if our code makes more calls to this API in the future
+    if (navigator.onLine) { // this checks if we are connected to a network/router, NOT if the browser can access the internet
+      $scope.show($ionicLoading);
+      $http.get("https://api.fixer.io/latest?base=USD").then(function (response) {
+        var data = response.data;
+        var allRates = data.rates;
+
+        var updatedOneUSDTo = {};
+        for (var key in oneUSDTo) {
+          var iRate = allRates[key];
+          if (iRate) {
+            updatedOneUSDTo[key] = iRate;
+          } else if (key === "USD") {
+            updatedOneUSDTo["USD"] = 1;
+          } else {
+            return;
+          }
+        }
+
+        oneUSDTo = updatedOneUSDTo;
+
+        var parseDateToUTC = function (dateStr) { //given "yyyy-mm-dd"
+          var parts = dateStr.split("-");
+          return new Date(Date.UTC(parts[0], parts[1]-1, parts[2], 15));
+        };
+
+        //API updates exchange rates at 4pm CET, which is 3pm UTC
+        $scope.exchangeRateLastUpdated = parseDateToUTC(data.date);
+      }, function (err) {
+
+      }).finally(function ($ionicLoading) {
+        // On both cases hide the loading
+        $scope.hide($ionicLoading);
+      });
+    }
+  };
+
+  $scope.getExchangeRates();
+
 }]);
