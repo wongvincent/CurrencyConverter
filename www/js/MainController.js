c9a540a3e42b71fab9947572162f1d54cee1779b
Vincent Wong <vincentw@live.ca>
2017-10-22 02:40:08 -0700

    Update foreign exchange rates

--- www/js/MainController.js
+++ www/js/MainController.js
@@ -139,41 +139,41 @@ angular.module('app')
              * as it just means copying and pasting the results of the API call.
              * */
             var oneUSDTo = {
-                "AUD": 1.2551,
-                "BGN": 1.6534,
-                "BRL": 3.1275,
-                "CAD": 1.2546,
-                "CHF": 0.96872,
-                "CNY": 6.7258,
-                "CZK": 22.088,
-                "DKK": 6.2883,
-                "GBP": 0.75598,
-                "HKD": 7.8165,
-                "HRK": 6.2611,
-                "HUF": 256.88,
-                "IDR": 13317.0,
-                "ILS": 3.5788,
-                "INR": 63.68,
-                "JPY": 110.73,
-                "KRW": 1123.8,
-                "MXN": 17.911,
-                "MYR": 4.2855,
-                "NOK": 7.9047,
-                "NZD": 1.3464,
-                "PHP": 50.318,
-                "PLN": 3.5962,
-                "RON": 3.8543,
-                "RUB": 60.703,
-                "SEK": 8.1087,
-                "SGD": 1.358,
-                "THB": 33.27,
-                "TRY": 3.5325,
-                "ZAR": 13.281,
-                "EUR": 0.84538
+                "AUD": 1.2739,
+                "BGN": 1.6549,
+                "BRL": 3.1747,
+                "CAD": 1.2494,
+                "CHF": 0.9813,
+                "CNY": 6.6185,
+                "CZK": 21.738,
+                "DKK": 6.2987,
+                "GBP": 0.75836,
+                "HKD": 7.803,
+                "HRK": 6.3518,
+                "HUF": 260.73,
+                "IDR": 13510.0,
+                "ILS": 3.4876,
+                "INR": 65.01,
+                "JPY": 113.17,
+                "KRW": 1130.6,
+                "MXN": 18.893,
+                "MYR": 4.225,
+                "NOK": 7.9662,
+                "NZD": 1.4288,
+                "PHP": 51.502,
+                "PLN": 3.5846,
+                "RON": 3.8899,
+                "RUB": 57.428,
+                "SEK": 8.1609,
+                "SGD": 1.3584,
+                "THB": 33.16,
+                "TRY": 3.663,
+                "ZAR": 13.684,
+                "EUR": 0.84617
             };
 
             // month is month-1. yyyy, mm, day, hours, minutes, seconds, milliseconds
-            $scope.exchangeRateLastUpdated = new Date(Date.UTC(2017, 7, 2, 15, 0, 0));
+            $scope.exchangeRateLastUpdated = new Date(Date.UTC(2017, 9, 20, 15, 0, 0));
 
             function createCurrenciesModel() {
                 $scope.currenciesModel = {};
