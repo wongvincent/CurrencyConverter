0f2e8bdb15eb7c528507a3eaf8a03f1770a95c0f
Vincent Wong <vincentw@live.ca>
2018-03-13 02:13:10 -0700

    Update foreign exchange rates, Add Icelandic Krona

--- www/js/MainController.js
+++ www/js/MainController.js
@@ -67,6 +67,10 @@ angular.module('app')
                     "show": false,
                     "fullname": "Indian Rupee"
                 },
+                "ISK": {
+                    "show": false,
+                    "fullname": "Icelandic Krona"
+                },
                 "JPY": {
                     "show": true,
                     "fullname": "Japanese Yen"
@@ -139,41 +143,42 @@ angular.module('app')
              * as it just means copying and pasting the results of the API call.
              * */
             var oneUSDTo = {
-                "AUD": 1.2739,
-                "BGN": 1.6549,
-                "BRL": 3.1747,
-                "CAD": 1.2494,
-                "CHF": 0.9813,
-                "CNY": 6.6185,
-                "CZK": 21.738,
-                "DKK": 6.2987,
-                "GBP": 0.75836,
-                "HKD": 7.803,
-                "HRK": 6.3518,
-                "HUF": 260.73,
-                "IDR": 13510.0,
-                "ILS": 3.4876,
-                "INR": 65.01,
-                "JPY": 113.17,
-                "KRW": 1130.6,
-                "MXN": 18.893,
-                "MYR": 4.225,
-                "NOK": 7.9662,
-                "NZD": 1.4288,
-                "PHP": 51.502,
-                "PLN": 3.5846,
-                "RON": 3.8899,
-                "RUB": 57.428,
-                "SEK": 8.1609,
-                "SGD": 1.3584,
-                "THB": 33.16,
-                "TRY": 3.663,
-                "ZAR": 13.684,
-                "EUR": 0.84617
+                "AUD":1.2714,
+                "BGN":1.5898,
+                "BRL":3.2586,
+                "CAD":1.2829,
+                "CHF":0.94952,
+                "CNY":6.3312,
+                "CZK":20.686,
+                "DKK":6.0556,
+                "EUR":0.81288,
+                "GBP":0.72013,
+                "HKD":7.8399,
+                "HRK":6.0519,
+                "HUF":253.53,
+                "IDR":13768.0,
+                "ILS":3.4443,
+                "INR":65.012,
+                "ISK":100.07,
+                "JPY":106.52,
+                "KRW":1066.7,
+                "MXN":18.682,
+                "MYR":3.9045,
+                "NOK":7.7748,
+                "NZD":1.37,
+                "PHP":52.081,
+                "PLN":3.4151,
+                "RON":3.7895,
+                "RUB":56.94,
+                "SEK":8.2556,
+                "SGD":1.3146,
+                "THB":31.33,
+                "TRY":3.8377,
+                "ZAR":11.848
             };
 
             // month is month-1. yyyy, mm, day, hours, minutes, seconds, milliseconds
-            $scope.exchangeRateLastUpdated = new Date(Date.UTC(2017, 9, 20, 15, 0, 0));
+            $scope.exchangeRateLastUpdated = new Date(Date.UTC(2018, 2, 12, 15, 0, 0));
 
             function createCurrenciesModel() {
                 $scope.currenciesModel = {};
