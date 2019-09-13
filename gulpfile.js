ebf2d62ad94ef2aacf40c477d400b56ed1d440a0
Vincent Wong <vincentw@live.ca>
2019-09-13 02:03:23 -0700

    Update some styles

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['/www/css/*.scss']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./www/css/*.scss')
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
